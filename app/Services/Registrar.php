1db1179516bc6656a71a38bb2c9aa016d0de2407
gieco <giecocartagena@outlook.com>
2015-07-07 22:48:37 +0000

    actualizar servicio registrar

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -16,18 +16,12 @@ class Registrar implements RegistrarContract {
 	{
 		return Validator::make($data, [
             'codigo' => 'required|max:16|unique:users',
-			'name' => 'required|max:255',
+			'name' => 'required|max:254',
 			'email' => 'required|email|max:255|unique:users',
 			'password' => 'required|confirmed|min:5',
 		]);
 	}
 
-	/**
-	 * Create a new user instance after a valid registration.
-	 *
-	 * @param  array  $data
-	 * @return User
-	 */
 	public function create(array $data)
 	{
 		return User::create([
