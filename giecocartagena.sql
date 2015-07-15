e2023e212b8d8b3ef205bbae80aaa2e808a6f48a
gieco <giecocartagena@outlook.com>
2015-07-15 13:47:03 +0000

    actualizacion de estructuras 1

--- giecocartagena.sql
+++ giecocartagena.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 07-07-2015 a las 21:27:29
+-- Tiempo de generación: 15-07-2015 a las 12:24:15
 -- Versión del servidor: 5.6.19-0ubuntu0.14.04.1
 -- Versión de PHP: 5.6.10-1+deb.sury.org~trusty+1
 
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `categorias`
 --
 
+DROP TABLE IF EXISTS `categorias`;
 CREATE TABLE IF NOT EXISTS `categorias` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(245) COLLATE utf8_unicode_ci NOT NULL,
@@ -39,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
-(1, 'Tratamientos', '0000-00-00 00:00:00', '2015-07-05 12:27:09');
+(1, 'Tratamientos', '0000-00-00 00:00:00', '2015-07-05 12:27:06');
 
 -- --------------------------------------------------------
 
@@ -47,6 +48,7 @@ INSERT INTO `categorias` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
 -- Estructura de tabla para la tabla `cms_noticias`
 --
 
+DROP TABLE IF EXISTS `cms_noticias`;
 CREATE TABLE IF NOT EXISTS `cms_noticias` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `titulo` varchar(245) COLLATE utf8_unicode_ci NOT NULL,
@@ -74,6 +76,7 @@ INSERT INTO `cms_noticias` (`id`, `titulo`, `encabezado`, `detalle`, `seccion_id
 -- Estructura de tabla para la tabla `cms_secciones`
 --
 
+DROP TABLE IF EXISTS `cms_secciones`;
 CREATE TABLE IF NOT EXISTS `cms_secciones` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(245) COLLATE utf8_unicode_ci NOT NULL,
@@ -97,6 +100,7 @@ INSERT INTO `cms_secciones` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
 -- Estructura de tabla para la tabla `datostratamiento`
 --
 
+DROP TABLE IF EXISTS `datostratamiento`;
 CREATE TABLE IF NOT EXISTS `datostratamiento` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
@@ -111,13 +115,22 @@ CREATE TABLE IF NOT EXISTS `datostratamiento` (
 -- Estructura de tabla para la tabla `eps`
 --
 
+DROP TABLE IF EXISTS `eps`;
 CREATE TABLE IF NOT EXISTS `eps` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `eps`
+--
+
+INSERT INTO `eps` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
+(1, 'COOMEVA', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'CAFESALUD', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -125,6 +138,7 @@ CREATE TABLE IF NOT EXISTS `eps` (
 -- Estructura de tabla para la tabla `estadiostumor`
 --
 
+DROP TABLE IF EXISTS `estadiostumor`;
 CREATE TABLE IF NOT EXISTS `estadiostumor` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
@@ -139,6 +153,7 @@ CREATE TABLE IF NOT EXISTS `estadiostumor` (
 -- Estructura de tabla para la tabla `fotos`
 --
 
+DROP TABLE IF EXISTS `fotos`;
 CREATE TABLE IF NOT EXISTS `fotos` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `imagen` varchar(245) COLLATE utf8_unicode_ci NOT NULL,
@@ -152,9 +167,88 @@ CREATE TABLE IF NOT EXISTS `fotos` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `genero`
+--
+
+DROP TABLE IF EXISTS `genero`;
+CREATE TABLE IF NOT EXISTS `genero` (
+  `codigo` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`codigo`, `nombre`) VALUES
+('F', 'FEMENINO'),
+('M', 'MACULINO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lugaresdiagnostico`
+--
+
+DROP TABLE IF EXISTS `lugaresdiagnostico`;
+CREATE TABLE IF NOT EXISTS `lugaresdiagnostico` (
+  `codigo` varchar(2) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `lugaresdiagnostico`
+--
+
+INSERT INTO `lugaresdiagnostico` (`codigo`, `nombre`) VALUES
+('1', 'CENDIPAT'),
+('2', 'UNIDAD DE PATOLOGIA CLINICA'),
+('3', 'MEDIPAT'),
+('4', 'HOSPITAL NAVAL'),
+('5', 'FIRE-MARTHA TUÑÓN'),
+('6', 'HOSPITAL UNIVERSITARIO DEL CARIBE'),
+('7', 'CLÍNICA EL BOSQUE');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `meses`
+--
+
+DROP TABLE IF EXISTS `meses`;
+CREATE TABLE IF NOT EXISTS `meses` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `meses`
+--
+
+INSERT INTO `meses` (`id`, `nombre`) VALUES
+(1, 'ENERO'),
+(2, 'FEBRERO'),
+(3, 'MARZO'),
+(4, 'ABRIL'),
+(5, 'MAYO'),
+(6, 'JUNIO'),
+(7, 'JULIO'),
+(8, 'AGOSTO'),
+(9, 'SEPTIEMBRE'),
+(10, 'OCTUBRE'),
+(11, 'NOVIEMBRE'),
+(12, 'DICIEMBRE');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `migrations`
 --
 
+DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
@@ -183,6 +277,7 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 -- Estructura de tabla para la tabla `password_resets`
 --
 
+DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE IF NOT EXISTS `password_resets` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -197,6 +292,7 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 -- Estructura de tabla para la tabla `productos`
 --
 
+DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(245) COLLATE utf8_unicode_ci NOT NULL,
@@ -220,9 +316,33 @@ INSERT INTO `productos` (`id`, `nombre`, `encabezado`, `detalle`, `fotoportada`,
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `regimenss`
+--
+
+DROP TABLE IF EXISTS `regimenss`;
+CREATE TABLE IF NOT EXISTS `regimenss` (
+  `codigo` varchar(2) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `regimenss`
+--
+
+INSERT INTO `regimenss` (`codigo`, `nombre`) VALUES
+('1', 'SUBSIDIADO'),
+('2', 'CONTRIBUTIVO'),
+('3', 'NO ASEGURADO'),
+('4', 'ESPECIAL');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `registropoblacional`
 --
 
+DROP TABLE IF EXISTS `registropoblacional`;
 CREATE TABLE IF NOT EXISTS `registropoblacional` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cedula` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
@@ -235,9 +355,11 @@ CREATE TABLE IF NOT EXISTS `registropoblacional` (
   `regimenseguridadsocial` varchar(90) COLLATE utf8_unicode_ci DEFAULT NULL,
   `eps` int(10) unsigned NOT NULL,
   `estrato` int(10) unsigned NOT NULL,
-  `iniciosintomas` date DEFAULT NULL,
+  `iniciosintomasanio` int(4) DEFAULT NULL,
+  `iniciosintomasmes` int(2) NOT NULL,
   `fechadiagnostico` date DEFAULT NULL,
   `metododiagnostico` varchar(90) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `otrotipomuestra` varchar(130) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `localizacionprimaria` varchar(90) COLLATE utf8_unicode_ci DEFAULT NULL,
   `morfologia` varchar(90) COLLATE utf8_unicode_ci DEFAULT NULL,
   `estadio` int(10) unsigned NOT NULL,
@@ -256,9 +378,58 @@ CREATE TABLE IF NOT EXISTS `registropoblacional` (
 -- Volcado de datos para la tabla `registropoblacional`
 --
 
-INSERT INTO `registropoblacional` (`id`, `cedula`, `iniciales`, `sexo`, `fechanacimiento`, `edad`, `residenciahabitual`, `lugarnacimiento`, `regimenseguridadsocial`, `eps`, `estrato`, `iniciosintomas`, `fechadiagnostico`, `metododiagnostico`, `localizacionprimaria`, `morfologia`, `estadio`, `lugardiganostico`, `numerobiopsia`, `lugartratamiento`, `datostratamiento`, `certificadodefuncion`, `created_at`, `updated_at`) VALUES
-(1, '73587852225', 'gc', 'M', '0000-00-00', 37, 'CARRERA 1, CALLE 2', 'CARTAGENA', 'CONTIBUTIVO', 0, 3, '0000-00-00', NULL, 'MATODO 1', 'DSSD', 'A', 1, 'A', '1', 'C', 0, '', '2015-07-07 10:16:57', '2015-07-07 12:19:53'),
-(2, '458552369', 'mo', 'F', '0000-00-00', 36, 'CARTAGENA', 'CICUCO', 'CONTIBUTIVO', 0, 2, '0000-00-00', NULL, 'METODO 2', 'LOCALIZADA', 'TIPO 1', 0, 'CLINICA 1', '525', 'ONCOLOGOS DEL CARIBE', 0, '', '2015-07-07 12:21:59', '2015-07-07 12:22:22');
+INSERT INTO `registropoblacional` (`id`, `cedula`, `iniciales`, `sexo`, `fechanacimiento`, `edad`, `residenciahabitual`, `lugarnacimiento`, `regimenseguridadsocial`, `eps`, `estrato`, `iniciosintomasanio`, `iniciosintomasmes`, `fechadiagnostico`, `metododiagnostico`, `otrotipomuestra`, `localizacionprimaria`, `morfologia`, `estadio`, `lugardiganostico`, `numerobiopsia`, `lugartratamiento`, `datostratamiento`, `certificadodefuncion`, `created_at`, `updated_at`) VALUES
+(1, '73587852225', 'gc', 'M', '0000-00-00', 37, 'CARRERA 1, CALLE 2', 'CARTAGENA', 'CONTIBUTIVO', 1, 3, 2004, 4, NULL, '3', '', 'DSSD', 'A', 1, '1', '1', 'C', 1, '', '2015-07-07 10:16:57', '2015-07-14 02:48:23'),
+(2, '458552369', 'mo', 'F', '0000-00-00', 36, 'CARTAGENA', 'CICUCO', 'CONTIBUTIVO', 2, 2, 0, 0, NULL, '4', 'MUESTRA ALEATORIA.. OTROS', 'LOCALIZADA', 'TIPO 1', 0, '4', 'ABC123456', 'ONCOLOGOS DEL CARIBE', 5, '', '2015-07-07 12:21:59', '2015-07-13 22:48:35'),
+(3, '45956235', 'MM', 'F', '2015-07-09', 26, 'CARRERA 1, CALLE 2', 'SANTA MARTA', NULL, 1, 1, 1956, 4, NULL, '3', '', 'XXX', 'YYYY', 0, '4', '56545', 'XXXXX', 5, '', '2015-07-14 16:47:20', '2015-07-15 03:37:43');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiposmuestras`
+--
+
+DROP TABLE IF EXISTS `tiposmuestras`;
+CREATE TABLE IF NOT EXISTS `tiposmuestras` (
+  `codigo` varchar(2) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tiposmuestras`
+--
+
+INSERT INTO `tiposmuestras` (`codigo`, `nombre`) VALUES
+('1', 'BIOPSIA INCISIONAL'),
+('2', 'BIOPSIA DECISIONAL'),
+('3', 'ESPÉCIMEN QUIRÚRGICO'),
+('4', 'OTROS');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamientos`
+--
+
+DROP TABLE IF EXISTS `tratamientos`;
+CREATE TABLE IF NOT EXISTS `tratamientos` (
+  `codigo` varchar(2) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tratamientos`
+--
+
+INSERT INTO `tratamientos` (`codigo`, `nombre`) VALUES
+('1', 'QUIMIOTERAPIA'),
+('2', 'RADIOTERAPIA'),
+('3', 'INMUNOTERAPIA'),
+('4', 'TERAPIA BLANCO'),
+('5', 'PALIATIVO'),
+('6', 'HORMONOTERAPIA');
 
 -- --------------------------------------------------------
 
@@ -266,6 +437,7 @@ INSERT INTO `registropoblacional` (`id`, `cedula`, `iniciales`, `sexo`, `fechana
 -- Estructura de tabla para la tabla `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `codigo` varchar(16) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
@@ -285,7 +457,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `codigo`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'Rafael Varela', 'rafa_varela@yahoo.com', '$2y$10$Bs5W1ZNMmgJ9gscP8fK5ae.GdHb2Sv0gKvx4ugQC8c40sRbVbE.r2', 'mXUUE2XxyXQAErB8phvjOZXK5LvZKLEghmhlOA8JnhbisdMPeTPEI6mg9zDF', '0000-00-00 00:00:00', '2015-07-07 13:57:48'),
+(1, 'admin', 'Rafael Varela', 'rafa_varela@yahoo.com', '$2y$10$Bs5W1ZNMmgJ9gscP8fK5ae.GdHb2Sv0gKvx4ugQC8c40sRbVbE.r2', '2kwkop3ayalf125jW1PmxNEOAhAyerSiJKqSWWDgA1yv4pABMJjFxlFWOG2o', '0000-00-00 00:00:00', '2015-07-08 11:15:29'),
 (10, 'RP100', 'Hospital Universitario', 'rp100@test.com', '$2y$10$LLo/oCBAzr9QL.aoD8/a..4YA5xDxVriU6VUV.4hKZ4XXleoCjESO', NULL, '2015-07-05 16:30:46', '2015-07-05 16:30:46'),
 (11, 'RP200', 'RP200', 'rp200@test.com', '$2y$10$v.sv4n/jxANpoo6aH1gaVuNVtjqo4.mWzqWq.Nqvz/ym89FVKQ5rW', NULL, '2015-07-05 16:32:01', '2015-07-05 16:32:01');
 
